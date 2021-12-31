8598669ed79e7575280f83784fa10417faf93b12
rycerz125 <jeste.oszuste.op.pl@gmail.com>
2021-12-31 16:12:54 +0100

    added gravity and bouncing pfysics

--- game.py
+++ game.py
@@ -1,24 +1,64 @@
 import pygame
 import os
 import time
+tiles = []
+
+class Hitbox(object):
+    def __init__(self, size, location):
+        self.size = size
+        self.bounceCoefficient = 0.5
+        self.location = location
+        self.maxLeft = location[0] - size/2
+        self.maxRight = location[0] + size/2
+        self.maxTop = location[1] - size/2
+        self.maxBottom = location[1] + size/2
+    def getBounceVector(self, hitbox):
+        self.maxLeft = self.location[0] - self.size/2
+        self.maxRight = self.location[0] + self.size/2
+        self.maxTop = self.location[1] - self.size/2
+        self.maxBottom = self.location[1] + self.size/2
+        if (self.maxBottom >= hitbox.maxTop and self.maxRight >= hitbox.maxLeft and self.maxLeft <= hitbox.maxRight):
+            print('vectorbounce')
+            return [1*self.bounceCoefficient,-1*self.bounceCoefficient]
+        return [1,1]
 
 class GameObject(object):
     def __init__(self, textureName,location):
         self.surface = pygame.image.load(os.path.join('textures', textureName))
         self.location = location
     def render(self, screen):
-        screen.blit(self.surface, self.location)
+        r = [self.location[0]-32, self.location[1]-32]
+        screen.blit(self.surface, r)
     def update(self):
         pass
 
 class Tile(GameObject):
-    pass
+    def __init__(self, size, location):
+        GameObject.__init__(self, size, location)
+        self.hitbox = Hitbox(64, self.location)
 
 class Bounce(GameObject):
+    def __init__(self, size, location):
+        GameObject.__init__(self, size, location)
+        self.velocityVector = [0,1]
+        self.hitbox = Hitbox(64, self.location)
+        self.gravity = 0.1
     def update(self):
-        self.location[1] += 1
+        if abs(self.velocityVector[1]) > 0.1:
+            self.velocityVector[1] += 0.1
+        self.location[1] += self.velocityVector[1]
+        self.location[0] += self.velocityVector[0]
+        if abs(self.velocityVector[0]) < 0.1:
+            self.velocityVector[0] = 0
+            self.gravity = 0
+        if abs(self.velocityVector[1]) < 0.1:
+            self.velocityVector[1] = 0
+            self.gravity = 0
+        for tile in tiles:
+            bounceVector = self.hitbox.getBounceVector(tile.hitbox)
+            self.velocityVector[0] *= bounceVector[0]
+            self.velocityVector[1] *= bounceVector[1]
 
-tiles = []
 def loadLevel(levelName):
     levelFile = open(os.path.join('levels', levelName), 'r')
     levelLines = levelFile.readlines()
@@ -69,5 +109,5 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-    time.sleep(0.1)
+    time.sleep(0.01)
     pygame.display.update()
