4776fa7a386f3e96acd22d9bc0ca0a105e376549
AyBiCi <rymcymcym100@gmail.com>
2021-12-31 14:51:28 +0000

    Added level loading

--- game.py
+++ game.py
@@ -18,6 +18,25 @@ class Bounce(GameObject):
     def update(self):
         self.location[1] += 1
 
+tiles = []
+def loadLevel(levelName):
+    levelFile = open(os.path.join('levels', levelName), 'r')
+    levelLines = levelFile.readlines()
+
+    x = 0
+    y = 0
+
+    for line in levelLines:
+        for c in line:
+            print("x: "+str(x)+" y: "+str(y))
+            if c == '1':
+                print("added tile on: x"+str(64*x)+" y: "+str(64*y))
+                tile = Tile('kafelek.png', [64*y,64*x])
+                tiles.append(tile)
+            y+=1
+        x+=1
+        y=0
+
 pygame.init()
 
 DISPLAY_X = 640
@@ -31,21 +50,20 @@ bounce = Bounce('bounce.png', [100,100] )
 def current_milli_time():
     return round(time.time() * 1000)
 
-tiles = []
-for i in range(5):
-    tile = Tile('kafelek.png', [64*i,64])
-    tiles.append(tile)
-
 measured = 0
+
+loadLevel('level1.lvl')
+
 while True:
     
     screen.fill((0,255,255))
 
-    bounce.update()
-    bounce.render(screen)
-
+    
     for tile in tiles:
         tile.render(screen)
+
+    bounce.update()
+    bounce.render(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
