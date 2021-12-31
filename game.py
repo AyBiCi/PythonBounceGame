4fca9d8f462a3194e2ce0bc690877d08462a0a41
rycerz125 <jeste.oszuste.op.pl@gmail.com>
2021-12-31 14:25:28 +0100

    added current time measuer + tile display

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import time
 
 class GameObject(object):
     def __init__(self, textureName,location):
@@ -8,7 +9,14 @@ class GameObject(object):
     def render(self, screen):
         screen.blit(self.surface, self.location)
     def update(self):
-        self.location[1] += 0.1
+        pass
+
+class Tile(GameObject):
+    pass
+
+class Bounce(GameObject):
+    def update(self):
+        self.location[1] += 1
 
 pygame.init()
 
@@ -18,17 +26,30 @@ DISPLAY_Y = 480
 screen = pygame.display.set_mode( (DISPLAY_X, DISPLAY_Y) )
 pygame.display.set_caption("Bounce")
 
-obj = GameObject('bounce.png', [100,100] )
+bounce = Bounce('bounce.png', [100,100] )
 
+def current_milli_time():
+    return round(time.time() * 1000)
+
+tiles = []
+for i in range(5):
+    tile = Tile('kafelek.png', [64*i,64])
+    tiles.append(tile)
+
+measured = 0
 while True:
+    
     screen.fill((0,255,255))
 
-    obj.update()
-    obj.render(screen)
+    bounce.update()
+    bounce.render(screen)
+
+    for tile in tiles:
+        tile.render(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-    
+    time.sleep(0.1)
     pygame.display.update()
